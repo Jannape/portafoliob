99cade0faedebd4fdfe6bf3cb1ffda9e9486564b
Jannape <yamileth_2005@hotmail.com>
2018-09-25 18:36:58 -0500

    subiendo portafolio

---
+++ arone/javascript.js
@@ -0,0 +1,13 @@
+
+  window.onload = function() {
+    let typewriter = document.getElementById('typewriter');
+    let h1=typewriter.getElementsByTagName("h1")[0];
+    h1.classList.add( "styletypewriter");
+
+  };
+ 
+
+
+
+
+
